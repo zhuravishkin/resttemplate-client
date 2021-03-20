8659ab0c7236543dcda83995040e24b6daf7605c
alexey.zhuravishkin <zhuravishkin@gmail.com>
2021-03-20 12:33:51 +0300

    added body for POST

--- src/main/java/com/zhuravishkin/resttemplateclient/controller/Controller.java
+++ src/main/java/com/zhuravishkin/resttemplateclient/controller/Controller.java
@@ -1,28 +1,35 @@
 package com.zhuravishkin.resttemplateclient.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zhuravishkin.resttemplateclient.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Slf4j
 @RestController
 @RequestMapping(value = "client")
 public class Controller {
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
-    public Controller(RestTemplate restTemplate) {
+    public Controller(RestTemplate restTemplate, ObjectMapper objectMapper) {
         this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping(value = "/get")
-    public ResponseEntity<User> get() {
+    public ResponseEntity<User> get() throws JsonProcessingException, URISyntaxException {
         try {
             throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "no body");
         } catch (HttpClientErrorException e) {
@@ -33,17 +40,25 @@ public class Controller {
             log.warn(e.getStatusCode().getReasonPhrase());
             log.warn(e.getMessage());
         }
-        String url = "http://localhost:8080/template";
+
+        String url = "http://localhost:8080/server";
+
         User getUser = restTemplate.getForEntity(url + "/get", User.class).getBody();
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(map, httpHeaders);
-        map.add("name", getUser.getName() + " Winchester");
-        int age = getUser.getAge();
-        map.add("age", String.valueOf(++age));
-        ResponseEntity<User> userResponseEntity = restTemplate.postForEntity(url + "/post", httpEntity, User.class);
-        log.info(userResponseEntity.getBody().toString());
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> httpEntity = new HttpEntity<>(objectMapper.writeValueAsString(getUser), httpHeaders);
+
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .uri(new URI(url))
+                .pathSegment("post")
+                .queryParam("surname", "Winchester")
+                .build()
+                .encode();
+
+        ResponseEntity<User> userResponseEntity = restTemplate.postForEntity(uriComponents.toString(), httpEntity, User.class);
+
+        log.warn(userResponseEntity.getBody().toString());
         return userResponseEntity;
     }
 }
